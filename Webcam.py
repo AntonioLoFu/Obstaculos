f4d3524b8772a38bfa0b61978eddf770f31f05ea
JuanAlberto <albertomarin14@hotmail.com>
2021-12-19 11:03:27 +0100

    funcion main

--- Webcam.py
+++ Webcam.py
@@ -1,23 +1,28 @@
 import cv2 
 
-vid = cv2.VideoCapture(0)
+VID = cv2.VideoCapture(0)
 
-while(1):
-      
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-      
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
+def __main__(camara = 0):
+    if camara != 0:
+        VID = cv2.VideoCapture(camara)
+    while(1):
+        
+        # Capture the video frame
+        # by frame
+        ret, frame = VID.read()
+    
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # After the loop release the cap object
+    VID.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+    __main__()
